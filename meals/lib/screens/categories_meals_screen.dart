2ee268e33519e65e12d75a35bdb4a670aaa3df1c
Marcos Vinicius Inocencio <marcos.inocencio13@gmail.com>
2020-11-30 10:55:27 -0300

    Refatoração
    Refatoração do app Expenses

---
+++ meals/lib/screens/categories_meals_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  CategoriesMealsScreen(this.category);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: Text('Receitas por Categoria ${category.id}'),
+      ),
+    );
+  }
+}
